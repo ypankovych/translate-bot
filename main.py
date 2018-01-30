17deae9069ee49b9efbd16befced2140d6f718f6
Pavel Durmanov <31005942+P-Alban@users.noreply.github.com>
2018-01-30 21:44:26 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import telebot
 bot = telebot.TeleBot(os.environ.get('token'))
 input_data = 'qwertyuiop[]asdfghjkl;\'zxcvbnm,./:`@#$^&"'
 output_data = 'йцукенгшщзхъфывапролджэячсмитьбю.жё"№;:?э'
-trans = str.maketrans(input_data, output_data)
+trans = str.maketrans(input_data + input_data.upper(), output_data + output_data.upper())
 
 
 @bot.message_handler(commands=['start'])
@@ -14,11 +14,10 @@ def start(message):
 
 @bot.message_handler(content_types=['text', 'video', 'photo', 'document'])
 def text_handle(message):
-    if message.reply_to_message and message.text == '/wat':
+    if message.reply_to_message and message.text.strip() == '/wat':
         response_text = getattr(message.reply_to_message, 'text') or getattr(message.reply_to_message, 'caption')
-        if not response_text or not any([x in input_data for x in response_text]):
-            return None
-        bot.reply_to(message.reply_to_message, text=response_text.lower().translate(trans))
+        if response_text and any([x in input_data for x in response_text.lower()]):
+            bot.reply_to(message.reply_to_message, text=response_text.translate(trans))
 
 
 if __name__ == '__main__':
